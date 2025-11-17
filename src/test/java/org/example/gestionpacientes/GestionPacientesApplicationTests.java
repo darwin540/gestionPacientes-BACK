cb0a4fbe5ac92269af53c60535b2fd6b4685a649
darwin540 <steween22@gmail.com>
2025-11-16 21:47:32 -0500

    se corrige pom y agrega file properties

---
+++ src/test/java/org/example/gestionpacientes/GestionPacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gestionpacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPacientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
